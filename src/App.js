e7deaf8f7b50f56042bc8877451d7a370560f360
Ujjwol Kayastha <uzol123@gmail.com>
2020-10-13 15:12:02 +0545

    feat: add routers and link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+//Routing
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Components
 import Header from "./components/Header";
 import Home from "./components/Home";
-import { GlobalStyle } from "./GlobalStyle";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <GlobalStyle />
-    </div>
-  );
-};
+//Styles
+import { GlobalStyle } from "./GlobalStyle";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+    <GlobalStyle />
+  </BrowserRouter>
+);
 
 export default App;
